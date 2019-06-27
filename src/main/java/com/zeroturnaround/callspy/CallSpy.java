c9eedbde9446bb1953010186dceb632c33f474af
ivankrylov <ivankrylov.java@gmail.com>
2019-06-27 18:49:22 +0300

    Modified example to add Inline annotation for a given class:method (hardcoded) This is just to test the theory that I can use an agent to appent (Force) inline attributes to arbitrary methods

--- src/main/java/com/zeroturnaround/callspy/CallSpy.java
+++ src/main/java/com/zeroturnaround/callspy/CallSpy.java
@@ -3,6 +3,9 @@ package com.zeroturnaround.callspy;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.*;
+import javassist.bytecode.*;
+import javassist.bytecode.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
@@ -10,6 +13,20 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class CallSpy implements ClassFileTransformer {
+
+  static void attachAttribute(CtClass cc, CtMethod method) {
+    ClassFile ccFile = cc.getClassFile();
+    ConstPool constpool = ccFile.getConstPool();
+    
+    // create the annotation
+    AnnotationsAttribute attr = new AnnotationsAttribute(constpool, AnnotationsAttribute.visibleTag);
+    Annotation annot = new Annotation("Inline", constpool);
+    //annot.addMemberValue("value", new IntegerMemberValue(ccFile.getConstPool(), 0));
+    attr.addAnnotation(annot);
+    method.getMethodInfo().addAttribute(attr); 
+  }
+
+
   @Override
   public byte[] transform(//region other parameters
                           ClassLoader loader,
@@ -20,34 +37,29 @@ public class CallSpy implements ClassFileTransformer {
                           byte[] classfileBuffer) throws IllegalClassFormatException {
 
     ClassPool cp = ClassPool.getDefault();
-    cp.importPackage("com.zeroturnaround.callspy");
-
-    //region filter agent classes
-    // we do not want to profile ourselves
-    if (className.startsWith("com/zeroturnaround/callspy")) {
-      return null;
-    }
-    //endregion
 
     //region filter out non-application classes
     // Application filter. Can be externalized into a property file.
     // For instance, profilers use blacklist/whitelist to configure this kind of filters
-    if (!className.startsWith("com/zt")) {
+    if (!className.startsWith("java/security/Provider")) {
       return classfileBuffer;
     }
+    System.out.println("Found class java.security.Provider");
     //endregion
 
     try {
       CtClass ct = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
 
+      // Add annotation
+      // https://stackoverflow.com/questions/2964180/adding-an-annotation-to-a-runtime-generated-method-class-using-javassist
+
       CtMethod[] declaredMethods = ct.getDeclaredMethods();
       for (CtMethod method : declaredMethods) {
         //region instrument method
-          method.insertBefore(" { " +
-                  "Stack.push();" +
-                  "Stack.log(\"" + className + "." + method.getName() + "\"); " +
-                  "}");
-          method.insertAfter("{ Stack.pop(); }", true);
+          if (method.getName().equals("getEngineName")) {
+              System.out.println("Attaching Force Inline attribute");
+              attachAttribute(ct, method);
+          }
         //endregion
       }
 
